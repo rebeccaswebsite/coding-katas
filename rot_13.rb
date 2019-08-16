e0b2edadfcd42b20dfbefc1c26ae3023daf65645
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-08-16 13:14:38 +0100

    Fix bug in solution, now letters past o are transformed too

--- rot_13.rb
+++ rot_13.rb
@@ -7,24 +7,46 @@
 # The function should return the new string with each letter transformed. 
 
 def rot13(string)
-    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  
-    new_string_arr = []
-  
-      string.split("").each do |i|
-        # Split string into an array of its characters
-        y = alphabet.index { |x| [i].include?(x) }
-        # For each character, the alphabet array is iterated over, to select the
-        # index of the element of the alphabet array that includes that character
-        # This index number is saved as variable y 
-        new_string_arr.push(alphabet[y+13])
-        # Outside the loop is an empty arrray, to push in the transformed letters
-        # To find the new letter, I look for the value in the alphabet array which has an 
-        # index 13 letters beyond the original letter
-      end
-  
-    new_string_arr.join('')
+  alphabet = {
+    'A' => 'N',
+    'B' => 'O',
+    'C' => 'P',
+    'D' => 'Q',
+    'E' => 'R',
+    'F' => 'S',
+    'G' => 'T',
+    'H' => 'U',
+    'I' => 'V',
+    'J' => 'W',
+    'K' => 'X',
+    'L' => 'Y',
+    'M' => 'Z',
+    'N' => 'A',
+    'O' => 'B',
+    'P' => 'C',
+    'Q' => 'D',
+    'R' => 'E',
+    'S' => 'F',
+    'T' => 'G',
+    'U' => 'H',
+    'V' => 'I',
+    'W' => 'J',
+    'X' => 'K',
+    'Y' => 'L',
+    'Z' => 'M'
+  }
 
-    # Join new_string_arr back together and return this final string
-  
+  new_string_arr = []
+
+    string.split("").each do |i|
+      # Split string into an array of its characters
+      new_string_arr.push(alphabet[i])
+      # Outside the loop is an empty arrray, to push in the transformed letters
+      # For each letter in the split string array, the value of the key with that same
+      # letter in the alphabet hash will be pushed into new_string_arr
+    end
+
+  new_string_arr.join('')
+  # Join new_string_arr back together and return this final string
+
 end
